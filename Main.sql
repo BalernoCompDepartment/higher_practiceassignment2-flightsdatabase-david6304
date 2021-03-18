c9f527d342ff848c659b4765f535611806c44917
David634 <david6304murray@gmail.com>
2021-03-18 12:32:59 +0000

    Finished

--- Main.sql
+++ Main.sql
@@ -9,8 +9,17 @@ adults pay £5.50
 children pay £2.00
 concessions pay £1.50 */
 
+SELECT forename, surname, (adultTicket*5.5+childTicket*2+concessionTicket*1.5) AS [Tax (£)]
+FROM Customer, Booking
+WHERE Customer.customerID='GR01932'
+AND Customer.customerID=Booking.customerID
+AND flightID='QH182';
 
 /* 2b(ii)  The airline wishes to identify the customer(s) who made a booking with the greatest number of children.
 Implement two SQL statements that will find the forename and surname of the customer(s) who made a booking with the greatest number of children. */
 
-
+SELECT forename, surname 
+FROM Customer, Booking, (SELECT MAX(childTicket) AS mostChildTickets
+FROM Booking)
+WHERE Customer.customerID=Booking.customerID
+AND childTicket=mostChildTickets;
